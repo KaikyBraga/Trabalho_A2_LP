7188beced203d0891f356b2b0e03778771a17e26
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-12-09 22:01:45 -0300

    atualizacoes

--- dino.py
+++ dino.py
@@ -1,19 +1,24 @@
 import os, sys, pygame, random
 
-WIDTH = 623
-HEIGHT = 150   
+from variaveis_globais import BRIDGE_PATH, WOODS_PATH
+
+WIDTH = 1200
+HEIGHT = 720   
+
+Y_FLOOR = 450
+Y_FLOOR_AVENTUREIRO = 450
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dino")
 
-class Dino():
+class Character():
     def __init__(self) -> None:
-        self.width = 44
-        self.height = 44
+        self.width = 50 * 5
+        self.height = 37 * 5
 
-        self.x= 30
-        self.y= 80
+        self.x = 30
+        self.y = Y_FLOOR_AVENTUREIRO - self.height//2
 
         self.jumping = False
         self.alpha = 1
@@ -21,9 +26,11 @@ class Dino():
         self.ddy = 0.75*(self.alpha**2)
 
         self.texture_num = 0
-        self.texture = [None, None, None]
-        self.mask = None
-        self.rect = None
+        self.texture_run = []
+        self.texture_jump = []
+        self.texture_dead = []
+        self.mask_run = []
+        self.mask_jump = []
         self.set_texture()
     
     def update(self):
@@ -31,76 +38,108 @@ class Dino():
             self.y -= self.dy
             self.dy -= self.ddy
 
-            if self.y >=80:
+            if self.y >= Y_FLOOR_AVENTUREIRO - self.height//2:
                 self.jumping = False
-                self.y=80
+                self.y = Y_FLOOR_AVENTUREIRO - self.height//2
+            
+            self.texture_num  = (min(self.texture_num + 0.25, len(self.texture_jump)-1))%len(self.texture_jump)
         else:
-            self.texture_num  = (self.texture_num + 0.25)%3
+            self.texture_num  = (self.texture_num + 0.25)%len(self.texture_run)
 
-        self.rect.x = self.x
-        self.rect.y = self.y
-    def show(self): 
-        screen.blit(self.texture[int(self.texture_num)], (self.x, self.y))
+    def show(self):
+        if self.jumping==False: 
+            screen.blit(self.texture_run[int(self.texture_num)], (self.x, self.y))
+        else:
+            screen.blit(self.texture_jump[int(self.texture_num)], (self.x, self.y))
 
     def set_texture(self):
-        for i in range(3):  
-            path = os.path.join(f'assets/images/dino{i}.png')
-            self.texture[i] = pygame.image.load(path)
-            self.texture[i] = pygame.transform.scale(self.texture[i], (self.width, self.height))
+        #carrega sprites de corrida
+        for i in range(1, 7):
+            aventureiro_run_path = f'sprites/personagens/aventureiro/corrida/aventureiro_corrida_{i}.png'  
+            path = os.path.join(aventureiro_run_path)
 
-        self.mask = pygame.mask.from_surface(self.texture[0])
-        self.rect = self.texture[0].get_rect()
+            img = pygame.image.load(path)
+
+            self.texture_run.append(pygame.transform.scale(img, (self.width, self.height)))
+            self.mask_run.append(pygame.mask.from_surface(self.texture_run[-1]))
+
+        #carrega sprites de pulo
+        for i in range(1, 5):
+            aventureiro_jump_path = f'sprites/personagens/aventureiro/pulo/aventureiro_pulo_{i}.png'  
+            path = os.path.join(aventureiro_jump_path)
+
+            img = pygame.image.load(path)
+
+            self.texture_jump.append(pygame.transform.scale(img, (self.width, self.height)))
+            self.mask_jump.append(pygame.mask.from_surface(self.texture_jump[-1]))
+        
+        #carrega sprites de morte
+        for i in range(1, 7):
+            aventureiro_dead_path = f'sprites/personagens/aventureiro/morte/aventureiro_morte_{i}.png'  
+            path = os.path.join(aventureiro_dead_path)
+
+            img = pygame.image.load(path)
+
+            self.texture_dead.append(pygame.transform.scale(img, (self.width, self.height)))
 
     def jump(self):
         if self.jumping==False:
-            self.dy=10*self.alpha
+            self.dy=17*self.alpha
             self.ddy = 0.75*(self.alpha**2)
             self.jumping = True
             self.texture_num=0
+    
+    def current_mask(self):
+        if self.jumping:
+            return self.mask_jump[int(self.texture_num)]
+        else:
+            return self.mask_run[int(self.texture_num)]
 
-class Cactus():
+class Bomb():
     def __init__(self, x) -> None:
-        self.width = 34
-        self.height = 44
+        self.width = 100
+        self.height = 100
 
-        self.x= x
-        self.y= 80
+        self.x = x
+        self.y = Y_FLOOR
 
         self.texture_num = 0
         self.texture = None
         self.mask = None
-        self.rect = None
 
         self.set_texture()
     
     def update(self, dx):
         self.x += dx
         
-        self.rect.x = self.x
-        self.rect.y = self.y
-
     def show(self): 
         screen.blit(self.texture, (self.x, self.y))
 
     def set_texture(self):
-        path = os.path.join(f'assets/images/cactus.png')
+        bomba_path = f'sprites/obstaculos/bomba/bomba.png'
+        path = os.path.join(bomba_path)
+
         self.texture = pygame.image.load(path)
         self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
 
         self.mask = pygame.mask.from_surface(self.texture)
-        self.rect = self.texture.get_rect()
+
 class BG:
-    def __init__(self, x=0) -> None:
+    def __init__(self, img_path, x=0, mult_speed=1) -> None:
         self.width = WIDTH
         self.height = HEIGHT
-        self.x = x
+
+        self.x = x  
         self.y = 0
         
+        self.mult_speed = mult_speed
+
+        self.img_path = img_path
         self.texture = None
         self.set_texture()
     
     def update(self, dx):
-        self.x += dx
+        self.x += int(dx*self.mult_speed)
         if self.x <= -self.width:
             self.x = self.width
 
@@ -108,37 +147,31 @@ class BG:
         screen.blit(self.texture, (self.x, self.y))
  
     def set_texture(self):
-        path = os.path.join('assets/images/bg.png')
+        path = os.path.join(self.img_path)
+
         self.texture = pygame.image.load(path)
         self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
 
 class Game():
     def __init__(self) -> None:
-        self.speed = 8
-        self.score = 0 
-        self.running = True
-
-        self.bg = [BG(0), BG(WIDTH)]
-        
-        self.dino = Dino()
-
-        self.obstacule = []
-        self.start_obstacle()
+        self.running = False
+        self.start_game()
 
     def update(self):
         self.score += 1
-        self.speed = 8 + 2*(self.score//100)
+        self.speed  = 8 + 2*(self.score//100) #dando erro
         self.dino.alpha = self.speed/5
 
     def start_game(self):
         if self.running==False:
-            self.score = 8
-            self.speed = 5
+            self.score = 0
+            self.speed = 15#8
             self.running = True
 
-            self.bg = [BG(0), BG(WIDTH)]
+            self.bg = [BG(WOODS_PATH, 0, 0.25), BG(WOODS_PATH, WIDTH, 0.25),
+                        BG(BRIDGE_PATH, 0), BG(BRIDGE_PATH, WIDTH)]
             
-            self.dino = Dino()
+            self.dino = Character()
 
             self.obstacule = []
             self.start_obstacle()
@@ -146,28 +179,27 @@ class Game():
     def check_colision(self):
         for obstacule in self.obstacule:
             pos = (obstacule.x-self.dino.x, obstacule.y-self.dino.y)
-            if self.dino.mask.overlap(obstacule.mask, pos)!=None:
+            if self.dino.current_mask().overlap(obstacule.mask, pos)!=None:
                 return True
         return False
     
     def start_obstacle(self):
-        self.obstacule.append(Cactus(WIDTH))
+        self.obstacule.append(Bomb(WIDTH))
         for i in range(2):
-            self.obstacule.append(
-                Cactus(random.randint(self.obstacule[-1].x+4*self.dino.width, self.obstacule[-1].x+WIDTH//1.5)))
+            x_min = self.obstacule[-1].x+1*self.dino.width
+            x_max = self.obstacule[-1].x+5*self.dino.width
+            self.obstacule.append(Bomb(random.randint(x_min, x_max)))
 
     def spawn_cactus(self):
         if self.obstacule[0].x <= -self.obstacule[0].width:
             self.obstacule.pop(0)
 
-            x_min = self.obstacule[-1].x + 4 * self.dino.width
-            x_max = self.obstacule[-1].x + WIDTH//1.5
+            x_min = self.obstacule[-1].x+1*self.dino.width
+            x_max = self.obstacule[-1].x+5*self.dino.width
 
             x_new_cactus = random.randint(x_min, x_max)
 
-            #print(x_max_obstaculo, x_new_cactus)
-
-            self.obstacule.append( Cactus(x_new_cactus) )
+            self.obstacule.append( Bomb(x_new_cactus) )
         
 def main():
 
@@ -189,12 +221,11 @@ def main():
                     game.start_game()
 
         if game.running:
+            screen.fill((255,255,255))
+
             for bg in game.bg:
                 bg.update(-game.speed)
                 bg.show()
-            
-            game.dino.update()
-            game.dino.show()
 
             game.spawn_cactus()
 
@@ -202,15 +233,18 @@ def main():
                 obstacule.update(-game.speed)
                 obstacule.show()
             
+            game.dino.update()
+            game.dino.show()
+
             if game.check_colision():
                 print("Colisao")
-                game.running = False
+                #game.running = False
 
             
             loop = (loop+1)%100
             
-            if(loop%2==0):
-                game.update()
+            #if(loop%2==0):
+            game.update()
 
             print(game.score)
 
--- variaveis_globais.py
+++ variaveis_globais.py
@@ -1,2 +1,5 @@
 LARGURA_TELA = 1280
 ALTURA_TELA = 720
+
+BRIDGE_PATH = 'sprites/cenario/ponte.png'
+WOODS_PATH = 'sprites/cenario/background.png'
